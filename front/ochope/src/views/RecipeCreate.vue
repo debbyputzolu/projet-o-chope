700c6d8ea00eba0f987504089b927695ffdd5580
debby-putzolu <debbymonchou@gmail.com>
2021-11-04 16:15:54 +0100

    recipeCreate page finish

--- front/ochope/src/views/RecipeCreate.vue
+++ front/ochope/src/views/RecipeCreate.vue
@@ -1,28 +1,32 @@
 <template>
 
     <section class="recipeCreate">
+        <img src="../assets/images/recipecreate.png" class="recipeCreateImage">
         <h1 class="recipeCreateTitle">Nouvelle recette</h1>
-        <form @submit="saveRecipe" class="recipeCreateForm">
+        <form @submit="handleSubmit" class="recipeCreateForm">
 
-            <div>
+            <div class="recipeCreateLabel">
                 <label>
-                    Titre de la recette <input v-model="title"/>
+                    Titre de la recette <br><input v-model="title"/>
                 </label>
+            </div>
+             <div class="error" v-if="titleEmpty">
+                Vous devez saisir un titre
             </div>
 
 
-            <div>
+            <div class="recipeCreateLabel">
                 <label>
-                    Image d'illustration <input type="file" @change="uploadImage"/>
+                    Image d'illustration <br><input type="file" @change="uploadImage"/>
                     <img v-if="image" :src="image"/>
                     <input v-model="imageId" type="hidden"/>
                 </label>
             </div>
 
 
-            <div>
+            <div class="recipeCreateLabel">
                 <div >
-                    <label> Type de recette
+                    <label> Type de recette<br>
                         <div>
   <input type="radio" id="huey" name="drone" value="huey"
          checked>
@@ -41,21 +45,27 @@
                     </label>
                 </div>
             </div>
-
-
-            <div>
-                <label>
-                    Description <textarea v-model="description"></textarea>
-                </label>
+             <div class="error" v-if="typeEmpty">
+                Vous devez sélectionner un type de recette
             </div>
 
 
-            <div>
-                <table id="array"> Ingrédients
-                <tr>
+            <div class="recipeCreateLabel">
+                <label>
+                    Description <br> <textarea v-model="description"></textarea>
+                </label>
+            </div>
+             <div class="error" v-if="descriptionEmpty">
+                Vous devez saisir une description
+            </div>
+
+            <label class="recipeCreateLabel">Ingrédients</label>
+            <div class="divIngredient">
+                <center><table id="array" > 
+                <tr class="recipeCreateTable">
                     <td>Nom</td><td>Quantité</td><td>Unité</td>
                 </tr>
-                <tr class = "ingredient-rows">
+                <tr class = "ingredient-rows recipeCreateTable">
                     <td>
                         <select>
                         <option>
@@ -73,21 +83,17 @@
                             <option>g</option>
                             <option>unité</option>
                         </select>
-                    </td>
-                </tr>
-            </table>
-            
-            <table>
-                <tr>
-                    <td>
                         <input type="button" id="add-row" name="add-row" value="+">
                     </td>
                 </tr>
-            </table>
+            </table></center>
+            </div>
+             <div class="error" v-if="ingredientEmpty">
+                Vous devez sélectionner un ou des ingredient(s)
             </div>
 
             <div>
-                <button>Enregistrer</button>
+                <button class="recipeCreateButton">Enregistrer</button>
             </div>
 
 
@@ -99,10 +105,84 @@
 </template>
 
 <script>
+import recipeService from '../services/recipeService.js';
+
 
 export default{
     name: 'RecipeCreate',
-   
+    data() {
+        return {
+            
+        title:'',
+        description:'',
+        types: [],
+        ingredients: [],
+        selectedTypes:null,
+        selectedIngredients:[],
+        picture:[],
+        titleEmpty: false,
+        descriptionEmpty: false,
+        ingredientEmpty: false,
+        typeEmpty: false, 
+        }
+    },
+    
+    
+    async handleSubmit(evt){
+          evt.preventDefault();
+          if(this.title == ''){
+              console.log(' TITRE EST VIDE !!');
+              this.titleEmpty = true;
+          }
+          if(this.stage == ''){
+              console.log('ETAPE EST VIDE !!');
+              this.stageEmpty = true;
+          }
+          if(this.selectedIngredients.length == 0){
+              console.log('INGREDIENT EST VIDE !!');
+              this.ingredientEmpty = true;
+          }
+          if(this.selectedTypes == null){
+              console.log('TYPE EST VIDE !!');
+              this.typeEmpty = true;
+          }
+         
+        
+          if(this.title != '' && 
+          this.selectedTypes != null &&
+          this.stage != '' &&
+          this.selectedIngredients.length != 0
+          ){
+            const result = await recipeService.saveRecipe(
+            this.title,
+            this.selectedTypes,
+            this.stage,
+            this.selectedIngredients
+            ); 
+          
+          
+          if(result){
+              this.$router.push('/');
+          }
+        }
+      },    
+      async uploadImage(evt){
+        evt.preventDefault();
+        
+    this.picture = this.ref;
+    console.log(this.picture);
+        const imageData = HTMLInputElement.files;
+        //console.log(imageData);
+        if(imageData.length != 0){
+         const result = await recipeService.uploadImage(
+            this.imageData
+            );  
+            
+            if(result){
+                console.log('image ok');
+            }
+        }
+      }
     
    
 }
