c94a663bdbe6589066cf4a9afa3a38c58a007d8c
debby-putzolu <debbymonchou@gmail.com>
2021-11-18 13:22:05 +0100

    responsive finish and page about finish

---
+++ front/ochope/src/views/About.vue
@@ -0,0 +1,38 @@
+<template>
+
+    <section class="about">
+        <h1 class="aboutTitle">A propos</h1>
+        <p class="aboutP">Notre site propose le partage de recette simple, originale que chaque apprenti brasseur pourra réaliser chez lui.</p>
+
+        <h2 class="aboutTeamSubtitle">La team</h2>
+        <div class="team">
+<div class="aboutTeam">
+    <img class="aboutTeamImage" src="../assets/images/aboutsyl.png" >
+    <h3 class="aboutTeamTitle">Sylvain<br>Product Owner</h3>
+</div>
+<div class="aboutTeam">
+    <h3 class="aboutTeamTitle">Thomas<br>Scrum Master</h3>
+    <img class="aboutTeamImage" src="../assets/images/aboutthom.png" >   
+</div>
+<div class="aboutTeam">  
+<img class="aboutTeamImage" src="../assets/images/aboutphil.png">
+  <h3 class="aboutTeamTitle">Philéas<br>Lead Dev Back</h3>
+</div>
+<div class="aboutTeam">
+    <h3 class="aboutTeamTitle">Debby <br>Lead Dev Front</h3>
+    <img class="aboutTeamImage" src="../assets/images/aboutdeb.png">
+</div>
+</div>
+    </section>
+
+</template>
+
+
+<script>
+
+export default({
+    name: 'TypeList',
+
+})
+
+</script>
